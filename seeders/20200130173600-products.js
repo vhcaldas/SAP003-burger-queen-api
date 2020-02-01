3a7be9aae23b3eb4bed5fd33bb9b8d84d6b3f75e
vhcaldas <vivianhcaldas@gmail.com>
2020-02-01 17:50:48 -0300

    Last changes in products table

--- seeders/20200130173600-products.js
+++ seeders/20200130173600-products.js
@@ -12,119 +12,119 @@ module.exports = {
       {
         name: 'Anéis de Cebola',
         price: 5,
-        type: product,
+        type: 'menuItem',
         createdAt: new Date(),
         updatedAt: new Date()
       },
       {
         name: 'Batata Frita',
         price: 5,
-        type: product,
+        type: 'menuItem',
         createdAt: new Date(),
         updatedAt: new Date()
       },
       {
         name: 'Café Americano',
         price: 5,
-        type: product,
+        type: 'menuItem',
         createdAt: new Date(),
         updatedAt: new Date()
       },
       {
         name: 'Café com Leite',
         price: 7,
-        type: product,
+        type: 'menuItem',
         createdAt: new Date(),
         updatedAt: new Date()
       },
       {
         name: 'Hambúrguer Duplo',
         price: 15,
-        type: product,
+        type: 'menuItem',
         createdAt: new Date(),
         updatedAt: new Date()
       },
       {
         name: 'Hambúrguer Simples',
         price: 10,
-        type: product,
+        type: 'menuItem',
         createdAt: new Date(),
         updatedAt: new Date()
       },
       {
         name: 'Misto Quente',
         price: 10,
-        type: product,
+        type: 'menuItem',
         createdAt: new Date(),
         updatedAt: new Date()
       },
       {
         name: 'Refrigerante 500ml',
         price: 7,
-        type: product,
+        type: 'menuItem',
         createdAt: new Date(),
         updatedAt: new Date()
       },
       {
         name: 'Refrigerante 750ml',
         price: 10,
-        type: product,
+        type: 'menuItem',
         createdAt: new Date(),
         updatedAt: new Date()
       },
       {
         name: 'Suco de Frutas Natural',
         price: 7,
-        type: product,
+        type: 'menuItem',
         createdAt: new Date(),
         updatedAt: new Date()
       },
       {
         name: 'Água 500ml',
         price: 5,
-        type: product,
+        type: 'menuItem',
         createdAt: new Date(),
         updatedAt: new Date()
       },
       {
         name: 'Água 750ml',
         price: 7,
-        type: product,
+        type: 'menuItem',
         createdAt: new Date(),
         updatedAt: new Date()
       },
       {
         name: 'Ovo',
         price: 1,
-        type: extras,
+        type: 'extras',
         createdAt: new Date(),
         updatedAt: new Date()
       },
       {
         name: 'Queijo',
         price: 1,
-        type: extras,
+        type: 'extras',
         createdAt: new Date(),
         updatedAt: new Date()
       },
       {
         name: 'Vegetariano',
         price: 0,
-        type: options,
+        type: 'options',
         createdAt: new Date(),
         updatedAt: new Date()
       },
       {
         name: 'Bovina',
         price: 0,
-        type: options,
+        type: 'options',
         createdAt: new Date(),
         updatedAt: new Date()
       },
       {
         name: 'Frango',
         price: 1,
-        type: options,
+        type: 'options',
         createdAt: new Date(),
         updatedAt: new Date()
       },
